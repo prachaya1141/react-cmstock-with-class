b40e729b04f2975dd5efdd2235241ef3cf2ee968
prachaya <prachaya.in@gmail.com>
2021-04-09 15:58:44 +0700

    formik

--- src/components/stockCreate/stockCreate.js
+++ src/components/stockCreate/stockCreate.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react'
-
+import { Formik } from 'formik';
 export default class stockCreate extends Component {
     render() {
         return (
-            <div>
-                stockCreate
-            </div>
+            <div className="content-wrapper">
+     <h1>Anywhere in your app!</h1>
+     <Formik
+       initialValues={{ email: '', password: '' }}
+       
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     >
+       {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+         <form onSubmit={handleSubmit}>
+           <input
+             type="email"
+             name="email"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+           />
+           {errors.email && touched.email && errors.email}
+           <input
+             type="password"
+             name="password"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.password}
+           />
+           {errors.password && touched.password && errors.password}
+           <button type="submit" disabled={isSubmitting}>
+             Submit
+           </button>
+         </form>
+       )}
+     </Formik>
+   </div>
         )
     }
 }
